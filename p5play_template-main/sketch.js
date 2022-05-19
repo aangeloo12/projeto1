e2b8a7e3b6bd3118cc77858ab07e33e41b1d8a39
aangeloo12 <105864929+aangeloo12@users.noreply.github.com>
2022-05-19 10:19:40 -0300

    primeiro projeto

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,17 @@
+var ball
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,60,60)
+  ball.shapeColor = "red"
+}
+
+function draw() 
+{
+  background("green");
+  drawSprites()
+
+}
+
+
+
+
